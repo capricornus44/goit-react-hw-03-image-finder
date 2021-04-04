16ca42621f0e2c6df9898024f388632bed04258a
capricornus44 <antonzavalniuk@gmail.com>
2021-04-04 15:13:57 +0200

    create initial project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from 'react';
+// import Searchbar from './components/Searchbar';
+// import ImageGallery from './components/ImageGallery';
+// import Button from './components/Button';
+// import Loader from './components/Loader';
+// import Modal from './components/Modal';
+// import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
 
-function App() {
-  return <div>App</div>;
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        {/* <Searchbar />
+        <ImageGallery />
+        <Button />
+        <Loader />
+        <Modal /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
