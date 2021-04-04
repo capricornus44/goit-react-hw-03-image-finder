47eb83528108448447ef2ba07ec295db492960e7
capricornus44 <antonzavalniuk@gmail.com>
2021-04-04 22:29:44 +0200

    Add class-component 'Modal' with  styles

--- src/App.js
+++ src/App.js
@@ -3,21 +3,48 @@ import React, { Component } from 'react';
 // import ImageGallery from './components/ImageGallery';
 // import Button from './components/Button';
 // import Loader from './components/Loader';
-// import Modal from './components/Modal';
+import Modal from './components/Modal';
 // import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
 
 class App extends Component {
-  state = {};
+  state = {
+    showModal: false,
+  };
+
+  componentDidMount() {
+    console.log('App componentDidMount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate');
+
+    // console.log(prevState)
+    // console.log(this.state)
+  }
+
+  // Function to open/close modal box
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
 
   render() {
+    const { showModal } = this.state;
+
     return (
       <div>
-        {/* <Searchbar />
-        <ImageGallery />
-        <Button />
-        <Loader />
-        <Modal /> */}
+        {/* <Searchbar /> */}
+        {/* <ImageGallery /> */}
+        {/* <Button /> */}
+        {/* <Loader /> */}
+
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src="" alt="" />
+          </Modal>
+        )}
       </div>
     );
   }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import scss from './Modal.module.scss';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log('Modal componentDidMount');
+
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    console.log('Modal componentWillUnmount');
+
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  // Function to close modal box by pushing ESCAPE
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={scss.overlay} onClick={this.handleBackdropClick}>
+        <div className={scss.modal}>
+          {/* <img src="" alt="" /> */}
+          {this.props.children}
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
+
+export default Modal;
