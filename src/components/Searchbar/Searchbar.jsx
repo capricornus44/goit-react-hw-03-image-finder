c6796d65ce3e1f00c4b9fe256010a725d533ce3a
capricornus44 <antonzavalniuk@gmail.com>
2021-04-05 22:58:47 +0200

    rename input attributes

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -36,8 +36,8 @@ class Searchbar extends Component {
           <input
             className={scss.searchFormInput}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
             value={this.state.searchQuery}
             onChange={this.handleChange}
